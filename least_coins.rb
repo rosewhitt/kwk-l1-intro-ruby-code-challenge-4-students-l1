fcd69501259e62ee9a4dc93661e3bcbb2debe800
rosewhitt <rosewhitt@icloud.com>
2018-06-06 23:23:03 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,20 +1,38 @@
 def least_coins(cents)
 
   coinhash = {
-    :quarters=> '0',
+    :quarter=> '0',
     :dime => '0',
     :nickel => '0',
     :penny => '0'}
 
-  numquarters = cents/25
-    if quarters == 0
-      coinhash[quarters] = numquarters
-        puts 'quarter => #{numquarters}'
-        
+  numquarter = cents/25
+    if quarter == 0
+      coinhash[quarter] = numquarter
+        puts 'quarter => #{numquarter}'
+    elsif
+      cents = cents - (quarter*25)
 
+  numdime = cents/10
+    if dime == 0
+      coinhash[dime] = numdime
+        puts 'dime => #{numdime}'
+    elsif
+      cents = cents - (dime*10)
 
+  numnickel = cents/5
+    if nickel == 0
+      coinhash[nickel] = numnickel
+    elsif
+      cents = cents - (nickel*5)
+        puts 'nickel => #{numnickel}'
 
-
-
-
+  numpenny = cents/1
+    if penny == 0
+      coinhash[penny] = numpenny
+    elsif
+      cents = cents - (penny*1)
+        puts 'penny => #{numpenny}'
 end
+
+least_coins(29)
