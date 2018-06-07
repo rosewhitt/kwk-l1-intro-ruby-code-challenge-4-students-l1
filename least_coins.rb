ed0b9884e46aeebbd83ecb3908c3269d80ec5d44
rosewhitt <rosewhitt@icloud.com>
2018-06-07 14:16:13 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -9,28 +9,28 @@ def least_coins(cents)
 
   numquarter = cents/25
     if numquarter == 0
-      coinhash[quarter] = numquarter
+      coinhash[:quarter] = numquarter
         puts 'quarter => #{numquarter}'
     elsif
       cents = cents - (quarter*25)
 
   numdime = cents/10
     if numdime == 0
-      coinhash[dime] = numdime
+      coinhash[:dime] = numdime
         puts 'dime => #{numdime}'
     elsif
       cents = cents - (dime*10)
 
   numnickel = cents/5
     if numnickel == 0
-      coinhash[nickel] = numnickel
+      coinhash[:nickel] = numnickel
     elsif
       cents = cents - (nickel*5)
         puts 'nickel => #{numnickel}'
 
   numpenny = cents/1
     if numpenny == 0
-      coinhash[penny] = numpenny
+      coinhash[:penny] = numpenny
     elsif
       cents = cents - (penny*1)
         puts 'penny => #{numpenny}'
